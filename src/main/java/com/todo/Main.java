0e9691ff23407db9066b22c318f53882459727f4
RAGA1111 <kit27.am40@gmail.com>
2025-09-16 14:17:22 +0530

    Added database connection feature

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,1 +1,17 @@
 
+package com.todo;
+import com.todo.util.DatabaseConnection;
+import java.sql.Connection;
+public class Main{
+
+    public static void main(String[] args) {
+        DatabaseConnection db_Connection = new DatabaseConnection();
+        try {
+            Connection cn = db_Connection.getDBConnection();
+            System.out.println("CONNECTED SUCCESSFULLY");
+        }
+        catch (Exception e) {
+            System.out.println("CONNECTION FAILED");
+        }
+    }
+}
